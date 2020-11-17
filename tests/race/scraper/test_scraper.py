6f9d5f516019518c1fde09fb2fb35e5a0c89de59
pndnism <p.dai.man8@gmail.com>
2020-11-18 00:18:31 +0900

    [add] raceinfoの相対パステストを追加

--- tests/race/scraper/test_scraper.py
+++ tests/race/scraper/test_scraper.py
@@ -37,3 +37,30 @@ class TestRaceInfoLisingPageScraper(TestCase):
         self.assertEqual(parser_mock.parse.call_args.args[0], "test html")
 
         self.assertEqual(result, expected_listing_page)
+
+    @responses.activate
+    def test_get_relative_path(self):
+
+        test_absolute_path = '%s?%s' % (NETKEIBA_BASE_URL, urlencode(RACE_LISTING_PAGE_POST_INPUT_DIC))
+        test_relative_path = urlencode(RACE_LISTING_PAGE_POST_INPUT_DIC)
+        responses.add(responses.GET, test_absolute_path, 
+                        body="test html", status=200)
+
+        parser_mock = Mock(spec=RaceInfoListingPageParser)
+        expected_listing_page = ListingPage(next_page_post_parameter=None,
+                                            next_page_element=None,
+                                            race_info_page_urls=[])
+
+        parser_mock.parse.return_value = expected_listing_page
+
+        listing_page_url = "?" + urlencode(RACE_LISTING_PAGE_POST_INPUT_DIC)
+        listing_page_requester = RaceInfoListingPageScraper(parser_mock)
+        result = listing_page_requester.get(listing_page_url)
+
+        self.assertEqual(len(responses.calls), 1)
+        self.assertEqual(responses.calls[0].request.url,
+                         '%s?%s' % (NETKEIBA_BASE_URL, urlencode(RACE_LISTING_PAGE_POST_INPUT_DIC)))
+
+        self.assertEqual(parser_mock.parse.call_args.args[0], "test html")
+
+        self.assertEqual(result, expected_listing_page)
