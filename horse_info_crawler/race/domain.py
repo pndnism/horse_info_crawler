9d4a5bf1030f22a46afa09d2ccf02744ef4481aa
pndnism <p.dai.man8@gmail.com>
2021-01-11 23:17:13 +0900

    [add] RaceDetailInfoのdomainを定義

--- horse_info_crawler/race/domain.py
+++ horse_info_crawler/race/domain.py
@@ -18,6 +18,44 @@ class RaceInfo:
     # DataFrameを使うというのは実際の実装に関わる部分なので、ここで定義するべきものではない。
     race_details: DataFrame
 
+@dataclass(frozen=True)
+class RaceDetailInfo:
+    # 着順
+    arrival_orders: List[str]
+    # 枠番
+    box_numbers: List[str]
+    # 馬番
+    horse_numbers: List[str]
+    # 馬情報
+    horse_info: List[str]
+    # 年齢&性別
+    horse_ages_and_sexes: List[str]
+    # 斤量
+    jockey_weights: List[str]
+    # 騎手
+    jockey_names: List[str]
+    # タイム
+    goal_times: List[str]
+    # 着差
+    goal_margins: List[str]
+    # 通過
+    order_transitions: List[str]
+    # 上り
+    half_times: List[str]
+    # 単勝オッズ
+    odds: List[str]
+    # 人気
+    popularities: List[str]
+    # 馬体重
+    horse_weights: List[str]
+    # 馬体重変化
+    horse_weight_diffs: List[str]
+    # 調教師
+    trainer_names: List[str]
+    # 馬主
+    horse_owners: List[str]
+    # 賞金
+    earn_prizes: List[str]    
 
 @dataclass(frozen=True)
 class ListingPage:
