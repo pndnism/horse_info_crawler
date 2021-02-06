be9730a5093ef710acc91dceeb8d595bbb8ed280
pndnism <p.dai.man8@gmail.com>
2021-02-07 02:23:44 +0900

    [add] domain.pyを追加

---
+++ horse_info_crawler/pedigree/domain.py
@@ -0,0 +1,40 @@
+
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass(frozen=True)
+class ListingPage:
+    next_page_url: Optional[str]
+    horse_info_page_urls: List[str]
+
+@dataclass(frozen=True)
+class HorseInfo:
+    name: str
+    birthday: Optional[str]
+    trainer_name: Optional[str]
+    owner_name: Optional[str]
+    producer: Optional[str]
+    origin_place: Optional[str]
+    mother: str
+    father: str
+    mother_of_father: str
+    father_of_father: str
+    mother_of_mother: str
+    father_of_mother: str
+
+
+@dataclass(frozen=True)
+class ShapedHorseInfo:
+    name: str
+    birthday: Optional[datetime]
+    trainer_name: Optional[str]
+    owner_name: Optional[str]
+    producer: Optional[str]
+    origin_place: Optional[str]
+    mother: str
+    father: str
+    mother_of_father: str
+    father_of_father: str
+    mother_of_mother: str
+    father_of_mother: str
